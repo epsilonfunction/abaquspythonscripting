c5fd6c898c23ca1393bac0532aaaef50cca035a6
Jia Yuan <foojiayuan@gmail.com>
2021-08-26 14:28:46 +0800

    hexagonal -only material creation

--- Main.py
+++ Main.py
@@ -41,72 +41,53 @@ global polygon_centres  #list
 global materials        #list
 global main_part        
 global base_part
+global set_list
 
 #Editable
 global global_length
 global global_matrix_size
 
 global_length = 1.0
-global_matrix_size = (5,5)
+global_matrix_size = (1,5)
 
-polygon_centres = polygon_matrix.matrix_centres(global_matrix_size[0],global_matrix_size[1],global_length) #only 2d squares for now
+polygon_centres = polygon_matrix.matrix_centres(
+    global_matrix_size[0],global_matrix_size[1],global_length,'hexagon')
 materials = material_creation_and_section.generate_materials(mp) #parameters fixed
 
-main_part = polygon_matrix.square(global_length,(0.0,0,0))
-polygon_creation.twod_rectangle(main_part[0],main_part[1],"Main0")
+new_hexagon_points=polygon_matrix.hexagon(global_length,(0.0,0.0))
+polygon_creation.twod_polygon("new",new_hexagon_points)
 
 base_part=[]
-base_part.append(["Main0",materials[0][2]])
-
-#Clone all materials in required dimension
+set_list=[]
 for i in range(len(materials)):
 
-    master = "Main0"
-    slave = "Part-"+str(i)
+    master="new"
+    slave="Part-"+materials[i][3]
 
     clone_parts.clone(master,slave)
-
-    base_part.append([slave,materials[i][2]])
-
-print(base_part) #sanity check
-
-for j in range(len(base_part)):
     
-    part_name,section_name = "",""
-    findAt_length = global_length*-0.166667
-
-    if j == 0:
-        part_name="Main0"
-        section_name="Section-0"
-
-    else:
-        part_name="Part-"+str(j-1)
-        section_name="Section-"+str(j-1)
-
-    SectionAssignment.SectionAssignment(
-        part_name,section_name,findAt_length
+    mdb.models['Model-1'].parts[slave].Set(
+        faces=mdb.models['Model-1'].parts[part_name].faces.findAt(
+            ((0,0,0),(0,0,0))
+        ),
+        name="set"+materials[i][2]
     )
 
-    base_part[j].append(section_name)
+    base_part.append([slave,materials[i[2]]])
 
-    meshing.meshing(
-        part_name,findAt_length
-    )
-
-print(base_part) #sanity check
+print(base_part)
 
 def binary_set(number):
     if number > 0:
         number = 0
-
     elif number == 0:
         number += 1
-
     return number
 
 binary_counter = 0
 counter = random.randrange(1,4)
 
+"""
 for i in range(len(polygon_centres)):
     counter += 1
 
@@ -114,10 +95,6 @@ for i in range(len(polygon_centres)):
         counter = 1
     
     if i == 0:
-
-        pass
-
-    else:
         
         slave_added = "Part-"+str(counter)
         master = "Main"+str(binary_counter)
@@ -125,48 +102,6 @@ for i in range(len(polygon_centres)):
         main_instance = "maininstance"
         slave_instance = "slaveinstance-Part#"+str(counter)
 
-        print("Now adding "+slave_instance+ "to "+main_instance)
-
-        # clone_parts.clone(master,new_part)
-
         mdb.models['Model-1'].rootAssembly.DatumCsysByDefault(CARTESIAN)
-
-        assem.create_instance(main_instance,master)
-        assem.create_instance(slave_instance,base_part[counter][0])
-
-        translation_cntr = list(polygon_centres[i])
-        translation_cntr.append(0.0)
-
-        vector_trans = tuple(translation_cntr)
-        assem.translate(slave_instance, vector_trans)
-
-        old_binary = binary_counter
-        binary_counter = binary_set(binary_counter)
-
-        assem.assembly_merge(main_instance,slave_instance,"Main"+str(binary_counter))
-
-        del mdb.models['Model-1'].parts['Main'+str(old_binary)]
-
-        #Problem of repeating instances | Not sure why. Seems to work fine either ways.
-        #Will like to cleanup soon.
-        try:
-            del mdb.models['Model-1'].rootAssembly.features['Main'+str(old_binary)+'-1']
-            print("Successful")
-        except:
-            print("Exception Occured")
-
-        print(slave_instance+" is added to "+main_instance)
-
-            
-
-
-
-
-
-#polygon_matrix.sketch_add(polygon_points)
-#print(polygon_points)
-
-#DONE: material_creation import | -DONE- on 4th Aug 2021. 
-#TODO: section_creation import
-#TODO: geometry_import
-
+        assem.create_instance(master,
+"""
--- polygon_matrix.py
+++ polygon_matrix.py
@@ -18,6 +18,9 @@ from sketch import *
 from visualization import *
 from connectorBehavior import *
 
+
+import numpy as np
+import math
 #import polygon_creation as poly_gen
 # class polygon_matrix(self):
 #     self.polygon_type=None #string?
@@ -34,28 +37,102 @@ from connectorBehavior import *
 #             for c in range(self.size[1]):
 #                 pass
 
-def matrix_centres(r,c,length):
-    #Creates centres of all origin
+def deg_to_rad(degrees):
+    radians = (degrees/180)*math.pi
+    return radians
+
+def vector_rotate(angle,vector):
+    #angle in degrees| int
+    #vector in array-like| list/tuple
+    radians = deg_to_rad(angle)
+    rotation_matrix = (
+        (math.cos(radians),-1*math.sin(radians)),
+        (math.sin(radians),   math.cos(radians))
+    )
+    new_vector = np.matmul(rotation_matrix,vector)
+    return new_vector
+
+def matrix_centres(r,c,length,polygon_type):
+    #Creates centres of all instances
     #Currently only square colony
     
     origin = (0.0,0.0) #must be in float for abaqus point generation to work
     output = []
+    output.append(origin)
 
-    for i in range(r):
-        for j in range(c):
+    if polygon_type=='square':
+
+        for i in range(r):
+            for j in range(c):
             
-            output.append(
+                output.append(
                     
                     (float(origin[0]+i*length),float(origin[1]+j*length))
 
-            )
+                )
     
-    return output
-  
+        return output
+    
+    elif polygon_type == 'hexagon':
+        vector = (0,0.86603*length*2)
+        counter = 0
+
+        def findallhexagoncenter(centre,vector):
+            vector_list = []
+            for i in range(6):
+                translation_vector=vector_rotate(30+i*60, vector)
+                new_centre=(
+                    float(round(centre[0]+translation_vector[0],5)),
+                    float(round(centre[1]+translation_vector[1],5))
+                )
+                vector_list.append(new_centre)
+            return vector_list
+
+        first_depth=findallhexagoncenter(origin,vector)
+        output.append(first_depth)
+
+    #     while counter <= r:
+    #         if counter == 0:
+    #             listing=findallhexagoncenter((0,0),vector)
+    #             # print(listing)
+    #             output.append(listing)
+
+    #         else:
+    #             to_add = []
+    #             check_list=[]
+    #             # print(output[counter-1])
+    #             for i in output[counter-1]:
+    #                 listing = findallhexagoncenter(i,vector)
+    #                 check_list.append(listing)
+
+    #             if counter == 1:
+    #                 for j in check_list:
+    #                     for k in j:
+    #                         if k not in output[counter-1] or not (0.0, 0.0):
+    #                             to_add.append(k)
+    #             else:
+    #                 for j in check_list:
+    #                     if j not in output[counter-1] or output[counter-2]:
+    #                         to_add.append(j)
+    #             output.append(to_add)
+
+    #         counter+=1
+    def flatten(l): #copied from http://rightfootin.blogspot.com/2006/09/more-on-python-flatten.html
+        out = []
+        for item in l:
+            if isinstance(item, (list)):
+                out.extend(flatten(item))
+            else:
+                out.append(item)
+        return out
+    a = flatten(output)
+    a = list(dict.fromkeys(a)) #remove duplicates
+
+    return a
 
 def square(length,centre):  
-    #takes in cartesian of origin and certain required length
-    #Outputs coordinates of all points
+    #takes in cartesian of origin and length of square
+    #Outputs coordinates of opposing ends in square for sketching
     
     shift = length/2
 
@@ -65,44 +142,35 @@ def square(length,centre):
     return [(new_x_0,new_y_0),
             (new_x_1,new_y_1)]
 
-def deg_to_rad(degrees):
-    radians = (degrees/180)*math.pi
-    return radians
+def rectangle(width,height,centre):
+    #takes in cartesian coordinates of origin and height and width of rectangle
+    #Outputs coordinates of opposing ends in square for sketching
 
-def hexagon(length,centre,angle):
-    #angle(in degrees): angle of tilt of first point wrt to centre
-    radians = [angle,60,60]
-    for i in range(3):
-        radians[i]=deg_to_rad(radians[i])
+    shift_x,shift_y=width/2,height/2
 
+    new_x_0,new_y_0 = float(centre[0]-shift_x),float(centre[1]-shift_y) #Bottom Left
+    new_x_1,new_y_1 = float(centre[0]+shift_x),float(centre[1]+shift_y) #Top Right
+
+
+
+def hexagon(length,centre):
+    #Rotation to be done after creation
+    #Creates points for sketching of hexagon
     output = []
 
     translation_vector_0 = [length,0]
     rotation_vector=[]
-
-    for j in radians:
-        rotation_vector.append(
-            (
-                (math.cos(j),-1*math.sin(j)),
-                (math.sin(j),   math.cos(j))
-            )
-        )   
     
-    translation_vector_1 = np.matmul(
-        rotation_vector[0],translation_vector_0
-    )
-    
-    point_1=(round(centre[0]+translation_vector_1[0],5),round(centre[1]+translation_vector_1[1],5))
+    point_1=(round(centre[0]+translation_vector_0[0],5),round(centre[1]+translation_vector_0[1],5))
     output.append(point_1)
-    translation_vector_2 = np.matmul(
-        rotation_vector[1],translation_vector_1
-    )
+
+    translation_vector_1 = vector_rotate(60,translation_vector_0)
+
     for k in range(5):
-        translation_vector_2 = np.matmul(
-            rotation_vector[2],translation_vector_2
-        )
+        translation_vector_1 = vector_rotate(60,translation_vector_1)
         point_1=(
-            round(point_1[0]+translation_vector_2[0],5),round(point_1[1]+translation_vector_2[1],5)
+            round(point_1[0]+translation_vector_1[0],5),
+            round(point_1[1]+translation_vector_1[1],5)
         )
     
         output.append(point_1)
